e33e9a138464285330edab4c1080c9b387dfb64d
ajaymaroju111 <marojuajay111@gmail.com>
2025-03-28 13:09:57 +0530

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 
+//imporing : 
+const authroutes = require('./src/controllers/authroutes.js')
+
 
 
 
 //initializing app : 
 const app = express();
 
-app
+app.use('/patiofy/user/', authroutes);
