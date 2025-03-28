48389a5597ce525012c48200f0b00f3b138b1369
maroju110125 <marojuajay@techpixe.com>
2025-03-28 14:32:53 +0530

    first commit

--- src/controllers/authroutes.js
+++ src/controllers/authroutes.js
@@ -9,7 +9,12 @@
 //user signup : 
 const signUp = async(req , res) =>{
   try {
-    
+    const { firstname, lastname, username , email, password} = req.body;
+    if(!firstname || !lastname || !username || !email || !password){
+      return res.status(400).json({
+        error : "All fields are required",
+      });
+    }
   } catch (error) {
     
   }
