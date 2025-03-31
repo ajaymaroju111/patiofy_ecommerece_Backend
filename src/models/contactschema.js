72017b4d93ffa32f0799c69398fba17a9c3c0f0b
maroju110125 <marojuajay@techpixe.com>
2025-03-31 16:05:18 +0530

    schemas added

---
+++ src/models/contactschema.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+
+const contactSchema = mongoose.Schema({
+  userId : {
+    type : mongoose.Schema.Types.ObjectId,
+    ref : 'users',
+  },
+  firstname : {
+    type : String,
+    required : [true , 'firstname is required'],
+  },
+  lastname : {
+    type : String,
+    required : [true , 'lastname is required'],
+  },
+  phone : {
+    type : String,
+    maxlength : 10,
+    minlength : 10,
+    required : [true , 'phone number is required'],
+  },
+  message : {
+    type : String,
+    required : [true , 'message is required']
+  }
+});
+
+module.exports = mongoose.model('contactform', contactSchema);
