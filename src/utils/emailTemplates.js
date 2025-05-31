32ac7c8b44d53ba1bdfdf6c962a282f39ccbb081
maroju110125 <marojuajay@techpixe.com>
2025-05-31 11:19:29 +0530

    changes

--- src/utils/emailTemplates.js
+++ src/utils/emailTemplates.js
@@ -108,7 +108,7 @@ exports.conformSignup = (username, securityKey) => {
         <h2>Verify Your Account</h2>
         <p class="username">Hello, <span id="user-name">${username}</span>!</p>
         <p>Click the button below to verify your email and activate your account.</p>
-        <a href="http://147.93.97.20:3000/patiofy/auth/user/verify?verificationKey=${securityKey}" class="verify-btn">Verify Account</a>
+        <a href="https://patiofy.comfortbikes.in/patiofy/auth/user/verify?verificationKey=${securityKey}" class="verify-btn">Verify Account</a>
     </div>
 
     <div class="company-info">
